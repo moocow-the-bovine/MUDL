c7c9a74d530a0d5a33c07e61e87bb36f962308a9
Bryan Jurish <jurish@bbaw.de>
2006-02-15 15:58:39 +0000

    + added MUDL::Gfsm.pm : quick load of MUDL::Gfsm::*
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL@1746 097f6656-c427-11dc-a790-1b5d5187385a

---
+++ MUDL/Gfsm.pm
@@ -0,0 +1,67 @@
+#-*- Mode: CPerl -*-
+
+## File: MUDL::Gfsm::Automaton
+## Author: Bryan Jurish <moocow@ling.uni-potsdam.de>
+## Description:
+##  + MUDL unsupervised dependency learner: Gfsm::Automaton wrapper
+##======================================================================
+
+package MUDL::Gfsm;
+
+use Gfsm;
+use MUDL::Gfsm::Automaton;
+use MUDL::Gfsm::Alphabet;
+
+use strict;
+
+
+1;
+
+##======================================================================
+## Docs
+=pod
+
+=head1 NAME
+
+MUDL - MUDL Unsupervised Dependency Learner
+
+=head1 SYNOPSIS
+
+ use MUDL;
+
+=cut
+
+##======================================================================
+## Description
+=pod
+
+=head1 DESCRIPTION
+
+...
+
+=cut
+
+##======================================================================
+## Footer
+=pod
+
+=head1 ACKNOWLEDGEMENTS
+
+perl by Larry Wall.
+
+=head1 AUTHOR
+
+Bryan Jurish E<lt>jurish@ling.uni-potsdam.deE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004, Bryan Jurish.  All rights reserved.
+
+This package is free software.  You may redistribute it
+and/or modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+perl(1)
+
+=cut
