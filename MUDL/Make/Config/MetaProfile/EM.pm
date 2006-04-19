3ae88d9703b5460289c2ba85d24ed5d784a7097b
Bryan Jurish <jurish@bbaw.de>
2006-04-19 10:01:48 +0000

    + added MUDL::Make::Config::MetaProfile::EM
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL@1798 097f6656-c427-11dc-a790-1b5d5187385a

---
+++ MUDL/Make/Config/MetaProfile/EM.pm
@@ -0,0 +1,161 @@
+##-*- Mode: CPerl -*-
+
+## File: MUDL::Make::Config::MetaProfile::EM.pm
+## Author: Bryan Jurish <moocow@ling.uni-potsdam.de>
+## Description:
+##  + MUDL unsupervised dependency learner: makefile configuration:
+##    : MUDL::Corpus::MetaProfile & descendants: EM
+##======================================================================
+
+package MUDL::Make::Config::MetaProfile::EM;
+use MUDL::Make::Config::MetaProfile;
+use MUDL::CmdUtils qw(load loadModule);
+use IO::File;
+use Carp;
+use strict;
+
+our @ISA = qw(MUDL::Make::Config::MetaProfile);
+
+##======================================================================
+## Globals
+our $DEBUG = 0;
+
+##======================================================================
+## OBJECT STRUCTURE: hash
+##  + inherited from MUDL::Make::Config
+##     ##
+##     ##-- Variables
+##     #vars  => $mudl_make_vars,
+##     uvars => $user_variables,
+##     xvars => \%expanded_vars,
+##     ##
+##     ##-- Targets
+##     dir      => $make_directory, ##-- directory in which to run make (default: '.')
+##     dirstack => \@stack,         ##-- directory stack
+##     targets  => $targets_str,    ##-- default: 'all'
+##
+##  + re-implemented MUDL::Make::Config::MetaProfile::EM from MUDL::Make::Config::MetaProfile
+##     eval_global  => $global_eval_summary, ##-- a MUDL::Corpus::Profile::ITagEval::Summary object
+##     eval_targets => $target_eval_summary, ##-- a MUDL::Corpus::Profile::ITagEval::Summary object
+##     mpsummary    => \%info,               ##-- as returned by $mp->getSummaryInfo() [base data]
+sub new {
+  my $that = shift;
+  my $self = bless $that->SUPER::new(
+				     ##-- Make targets
+				     targets  => 'em-global-eval-summary em-tgs-eval-summary',
+				     userfile => 'user.mak',
+
+				     ##-- acquisition data
+				     eval_global  => undef,
+				     eval_targets => undef,
+				     mpsummary    => undef,
+
+				     ##-- User args
+				     @_
+				    ), ref($that)||$that;
+  return $self;
+}
+
+## $cfg = $cfg->clear;
+sub clear {
+  my $cfg = shift;
+  $cfg->SUPER::clear();
+
+  $cfg->{targets} = 'em-global-eval-summary em-tgs-eval-summary';
+  $cfg->{userfile} = 'user.mak';
+  delete(@$cfg{qw(eval_global eval_targets mpsummary)});
+
+  return $cfg;
+}
+
+
+##======================================================================
+## Make: Acquire Data
+
+## $bool = $cfg->acquire(%args)
+##  + called after successful $cfg->make()
+##  + may be implemented in child classes for data acquisition
+##  + the process is already chdir()d to the $cfg->{dir} when this method is called!
+sub acquire {
+  my ($cfg,%args) = @_;
+
+  ##-- get common data
+  my $mpdir = $cfg->{xvars}{mpdir};
+  my $stage = $cfg->{xvars}{stage};
+  my $emi   = $cfg->{xvars}{emi};
+  my $emdir = $cfg->{xvars}{emdir};
+
+  ##-- acquire Config::MetaProfile eval data
+  $cfg->SUPER::acquire(%args);
+  @$cfg{qw(mpeval_global mpeval_targets)} = @$cfg{qw(eval_global eval_targets)};
+
+  ##-- get basename for summary files
+  my $filebase = $cfg->{xvars}{emhmm};
+  $filebase  =~ s/\.bin$//;
+  $filebase .=  ".t-".$cfg->{xvars}{tbase};
+
+  ##-- load eval (summary): global
+  my ($file);
+  $file = "${filebase}.eval.summary.bin";
+  $cfg->{eval_global} = MUDL::Corpus::Profile::ITagEval->loadFile($file)
+    or confess(ref($cfg),"::acquire(): could not load global-eval file '$file': $!");
+
+  ##-- load eval (summary): targets
+  $file = "${filebase}.tgs.eval.summary.bin";
+  $cfg->{eval_targets} = MUDL::Corpus::Profile::ITagEval->loadFile($file)
+    or confess(ref($cfg),"::acquire(): could not load target-eval file '$file': $!");
+
+  return $cfg;
+}
+
+
+1;
+
+##======================================================================
+## Docs
+=pod
+
+=head1 NAME
+
+MUDL - MUDL Unsupervised Dependency Learner
+
+=head1 SYNOPSIS
+
+ use MUDL;
+
+=cut
+
+##======================================================================
+## Description
+=pod
+
+=head1 DESCRIPTION
+
+...
+
+=cut
+
+##======================================================================
+## Footer
+=pod
+
+=head1 ACKNOWLEDGEMENTS
+
+perl by Larry Wall.
+
+=head1 AUTHOR
+
+Bryan Jurish E<lt>jurish@ling.uni-potsdam.deE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006, Bryan Jurish.  All rights reserved.
+
+This package is free software.  You may redistribute it
+and/or modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+perl(1)
+
+=cut
