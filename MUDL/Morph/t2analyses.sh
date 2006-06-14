4bafca8bb47ce111d14caf25baefe68799ae999a
Bryan Jurish <jurish@bbaw.de>
2006-06-14 08:18:22 +0000

    *** empty log message ***
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL@1816 097f6656-c427-11dc-a790-1b5d5187385a

--- MUDL/Morph/t2analyses.sh
+++ MUDL/Morph/t2analyses.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+
+#fst=/usr/local/share/moot/mootm-stts-lemmas-nocase-initial.gfst.gz
+#lab=/usr/local/share/moot/mootm-stts.lab
+##--
+fst=mootm-stts-lemmas-nocase-initial.gfst.gz
+lab=mootm-stts.lab
+
+##-- MAIN
 tfile="$1"
 test -z "$tfile" && tfile="-"
 
 tab=`echo -e "\t"`
+
 
 cat "$tfile" \
  | cut -f 1 \
@@ -13,7 +22,8 @@ cat "$tfile" \
  | uniq \
  | recode utf8..latin1 \
  | mootm \
-     -s/usr/local/share/moot/mootm-stts.lab \
-     -m/usr/local/share/moot/mootm-stts-lemmas-nocase-initial.gfst.gz \
+     -a \
+     -s"$lab" \
+     -m"$fst" \
  | grep "${tab}." \
  | recode latin1..utf8
