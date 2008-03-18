d86673eec6807698a3224137257c268b949f29b7
Bryan Jurish <jurish@bbaw.de>
2008-03-18 08:21:09 +0000

    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL/MUDL/Corpus@2006 097f6656-c427-11dc-a790-1b5d5187385a

---
+++ Profile/LRJointP.pm
@@ -0,0 +1,89 @@
+#-*- Mode: CPerl -*-
+
+## File: MUDL::Corpus::Profile::LRJointP.pm
+## Author: Bryan Jurish <moocow@ling.uni-potsdam.de>
+## Description:
+##  + MUDL corpus profile: L-R joint (target,bound) probabilities
+##======================================================================
+
+package MUDL::Corpus::Profile::LRJointP;
+use MUDL::Corpus::Profile::LRBigrams;
+use MUDL::Object;
+use MUDL::EDist;
+use PDL;
+use Carp;
+use strict;
+our @ISA = qw(MUDL::Corpus::Profile::LRBigrams); #)
+
+##======================================================================
+## $lr = $class_or_obj->new(%args)
+##   + %args:
+##       eos => $eos_str,
+##       bos => $bos_str,
+##       bounds => $bounds_enum,
+##       targets => $targets_enum,
+##       left=>$left_bigrams,       ## ($target,$lneighbor)
+##       right=>$right_bigrams,     ## ($target,$rneighbor)
+##       smoothgt=>$which,          ## either false or 'bigrams'
+sub new {
+  my ($that,%args) = @_; 
+  return $that->SUPER::new(nfields=>1,donorm=>0,%args);
+}
+
+##======================================================================
+## Profiling
+
+## undef = $profile->addSentence(\@sentence)
+##  + inherited
+
+## undef = $profile->addBigrams($bigrams,%args)
+##  + inherited
+
+##======================================================================
+## Conversion: to PDL
+
+##-- inherited from MUDL:::Corpus::Profile::LR
+
+## $pdl = $lr->toPDL()
+## $pdl = $lr->toPDL($pdl)
+
+## $pdl3d = $lr->smoothPdl($pdl3d);
+##-- inherited
+
+## $pdl3d = $lr->finishPdl($pdl3d);
+sub finishPdl {
+  my ($lr,$pdl,%args) = @_;
+  @$lr{keys %args} = values %args;   ##-- args: clobber
+
+  ##-- get global total
+  my $N = $lr->{ftotal};
+  $pdl /= $N;
+
+  return $pdl;
+}
+
+## undef = $profile->finishPdlProfile(%args)
+##  + perform pdl-sensitive finishing actions
+#-- inherited
+
+
+##======================================================================
+## Help
+
+## $string = $class_or_obj->helpString()
+sub helpString {
+  my $that = shift;
+  return
+    (qq(Extract left- and right- joint-probability profile wrt. fixed boundary set.\n)
+     .qq(Options:\n)
+     .qq(  bounds=ENUM      [default=empty]\n)
+     .qq(  targets=ENUM     [default=empty]\n)
+     .qq(  eos=EOS_STRING   [default='__\$']\n)
+     .qq(  bos=BOS_STRING   [default='__\$']\n)
+     .qq(  donorm=BOOL      [default=1]\n)
+     .qq(  smoothgt=WHICH   [false or "bigrams"; default=0]\n)
+    );
+}
+
+
+1;
