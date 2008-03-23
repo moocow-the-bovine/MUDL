ad3ca6255e981d82b5ea2b7aaca4f15f2534091b
Bryan Jurish <jurish@bbaw.de>
2008-03-23 21:47:59 +0000

    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL@2011 097f6656-c427-11dc-a790-1b5d5187385a

---
+++ MUDL/PDL/Stats.pm
@@ -0,0 +1,83 @@
+#-*- Mode: CPerl -*-
+
+## File: MUDL::PDL::Stats.pm
+## Author: Bryan Jurish <moocow@ling.uni-potsdam.de>
+## Description:
+##  + MUDL: PDL statistical utilities
+##======================================================================
+
+package MUDL::PDL::Stats;
+use PDL;
+
+use strict;
+
+##======================================================================
+## Variance, standard deviation
+
+## $mean = $pdl->mean()
+BEGIN {
+  *mean = *PDL::mean = \&PDL::average;
+}
+
+## $var = $pdl->variance()
+BEGIN { *PDL::variance = \&variance; }
+sub variance {
+  my $p = shift;
+  return ($p**2)->average - $p->average->slice("*1");
+}
+
+## $stddev = $pdl->stddev()
+BEGIN { *PDL::stddev = \&stddev; }
+sub stddev { return $_[0]->variance->sqrt(); }
+
+
+1;
+
+##======================================================================
+## Docs
+=pod
+
+=head1 NAME
+
+MUDL - MUDL Unsupervised Dependency Learner
+
+=head1 SYNOPSIS
+
+ use MUDL;
+
+=cut
+
+##======================================================================
+## Description
+=pod
+
+=head1 DESCRIPTION
+
+...
+
+=cut
+
+##======================================================================
+## Footer
+=pod
+
+=head1 ACKNOWLEDGEMENTS
+
+perl by Larry Wall.
+
+=head1 AUTHOR
+
+Bryan Jurish E<lt>jurish@ling.uni-potsdam.deE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004, Bryan Jurish.  All rights reserved.
+
+This package is free software.  You may redistribute it
+and/or modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+perl(1)
+
+=cut
