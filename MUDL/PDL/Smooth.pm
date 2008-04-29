e980556edbdc9aface35430cdbac3aec081e890a
Bryan Jurish <jurish@bbaw.de>
2008-04-29 20:59:57 +0000

    + added basic linear interpolation stuff: still looks like crap (maybe with D() ?
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL@2072 097f6656-c427-11dc-a790-1b5d5187385a

--- MUDL/PDL/Smooth.pm
+++ MUDL/PDL/Smooth.pm
@@ -369,34 +369,39 @@ sub uosm {
 ##  + finds $lambdas = pdl([$lambda1,$lambda2]) for estimating values of dim=1 of $f12
 ##    by $f2 independent probabilities ($lambda2) or dim=0 conditional probabilities ($lambda12)
 ##  + %args:
-##     f1 => $f1, ##-- optional, dense or CCS::Nd
-##     f2 => $f2, ##-- optional, dense or CCS::Nd
-##     N  => $N,  ##-- optional
+##     f1    => $f1,  ##-- optional, dense or CCS::Nd
+##     f2    => $f2,  ##-- optional, dense or CCS::Nd
+##     N     => $N,   ##-- optional
+##     hapax => $how, ##-- '12':add to $lambda12, ('1' or '2'):add to $lambda2, anything else: ignore (default)
 BEGIN { *PDL::diLambdas2 = *PDL::CCS::Nd::diLambdas2 = \&diLambdas2; }
 sub diLambdas2 {
   my ($f12,%args) = @_;
   my $f1  = defined($args{f1}) ? $args{f1} : $f12->xchg(0,1)->sumover;
   my $f2  = defined($args{f2}) ? $args{f2} : $f12->sumover;
   my $N   = defined($args{N})  ? $args{N}  : $f2->sumover;
+  my $hapax_how = defined($args{hapax})  ? $args{hapax}  : 'ignore';
   #$f12 = $f12->toccs if (!UNIVERSAL::isa($f12,'PDL::CCS::Nd'));
   $f1  = $f1->decode if (UNIVERSAL::isa($f1,'PDL::CCS::Nd'));
   $f2  = $f2->decode if (UNIVERSAL::isa($f2,'PDL::CCS::Nd'));
 
   my $f12i = $f12->whichND;
-  my $f12v = UNIVERSAL::isa($f12,'PDL::CCS::Nd') ? $f12->whichVals : $f12->indexND($f12i);
-  my $f1v  = $f1->index( $f12i->slice("(0),") );
-  my $f2v  = $f2->index( $f12i->slice("(1),") );
+  my $f12v = UNIVERSAL::isa($f12,'PDL::CCS::Nd') ? $f12->whichVals->double : $f12->indexND($f12i)->double;
+  my $f1v  = $f1->double->index( $f12i->slice("(0),") );
+  my $f2v  = $f2->double->index( $f12i->slice("(1),") );
 
   my $p12v = ($f12v-1) / ($f1v-1);
   my $p2v  = ($f2v-1)  / ($N-1);
 
-  my $f1_hapax_mask  = ($f1v  <= 1);
-  #$p12v->where($f1_hapax_mask) .= 0; ##-- prefer independent p2 for hapax
-  $p12v->where($f1_hapax_mask) .= 1;  ##-- prefer conditional p12 for hapax
+  my $f1_hapax_mask    = ($f1v <= 1);
+  my $f1_nonhapax_mask = $f1_hapax_mask->not;
 
   ##-- compare
-  my $p12_best = ($p12v >= $p2v);
-  my $p2_best  = $p12_best->not;
+  my $p12_best = $f1_nonhapax_mask & ($p12v > $p2v);
+  my $p2_best  = $f1_nonhapax_mask & $p12_best->not;
+
+  ##-- apply hapax preference
+  $p12_best |= $f1_hapax_mask if ($hapax_how eq '12');
+  $p2_best  |= $f1_hapax_mask if ($hapax_how eq '1' || $hapax_how eq '2');
 
   ##-- extract frequencies
   my $p12_best_f = $f12v->where($p12_best);
@@ -406,7 +411,12 @@ sub diLambdas2 {
   my $lambda12f = $p12_best_f->sumover;
   my $lambda2f  = $p2_best_f->sumover;
   my $lambdas   = pdl(PDL::double, [$lambda2f,$lambda12f]);
-  $lambdas     /= $lambdas->sumover;
+  if (!all($lambdas==0)) {
+    $lambdas /= $lambdas->sumover;
+  } else {
+    ##-- all ignored hapax: hack
+    $lambdas .= $lambdas->ones / 2;
+  }
 
   return wantarray ? $lambdas->dog : $lambdas;
 }
