b22f772e4aa05fef3ae6757737baa0580b187e3c
Bryan Jurish <jurish@bbaw.de>
2009-07-14 10:19:50 +0000

    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/MUDL@2126 097f6656-c427-11dc-a790-1b5d5187385a

--- MUDL/Corpus/Profile/LRRPMI.pm
+++ MUDL/Corpus/Profile/LRRPMI.pm
@@ -1,10 +1,14 @@
 #-*- Mode: CPerl -*-
 
-## File: MUDL::Corpus::Profile::LRPMI.pm
+## File: MUDL::Corpus::Profile::LRRPMI.pm
 ## Author: Bryan Jurish <moocow@ling.uni-potsdam.de>
 ## Description:
 ##  + MUDL unsupervised dependency learner: corpus profile:
-##    L-R pointwise mutual information
+##    L-R relative pointwise mutual information
+##
+##  + prof(w,b) ~= i(w;b)             / h(w,b)
+##              := (h(w)+h(b)-h(w,b)) / h(w,b)  , if f(w,b)>0
+##              := 0                            , otherwise
 ##======================================================================
 
 package MUDL::Corpus::Profile::LRPMI;
@@ -23,18 +27,12 @@ our @ISA = qw(MUDL::Corpus::Profile::LRBigrams);
 ##       targets => $targets_enum,
 ##       left=>$left_bigrams,       ## ($target,$lneighbor)
 ##       right=>$right_bigrams,     ## ($target,$rneighbor)
+##       log_eps=>$eps,             ## used as frequency smoother, default=0
 sub new {
   my ($that,%args) = @_; 
-  return $that->SUPER::new(nfields=>1,donorm=>1,%args);
+  return $that->SUPER::new(donorm=>0,dolog=>0,log_eps=>0,%args);
   return $self;
 }
-
-##======================================================================
-## Profiling
-
-## undef = $profile->addSentence(\@sentence)
-##  + inherited
-
 
 ##======================================================================
 ## Conversion: to PDL
@@ -43,16 +41,23 @@ sub new {
 ##   + $pdl3d : (2, $nbds, $ntgs)
 sub finishPdl {
   my ($lr,$pdl) = @_;
+  @$lr{keys %args} = values %args;   ##-- args: clobber
 
-  my ($Ptb, $Pt, $Pb);
-  foreach my $dir (0,1) {
-    $Ptb  = $pdl->slice("($dir),,");
-    $Ptb /= $Ptb->sum;
+  ##-- get common data: frequencies
+  my $N  = pdl(double,$lr->{ftotal});
+  my $fw = $lr->{ptugs}{pdl}->double;  ##--  [w]   -> f(w)
+  my $fb = $lr->{pbugs}{pdl}->double;  ##--  [b]   -> f(b)
+  my $eps = $lr->{log_eps}||0;
 
-    $Pt  = $Ptb->sumover->transpose;
-    $Pb  = $Ptb->xchg(0,1)->sumover;
+  ##-- get common data: entropies
+  my $hw  = -log2(($fw+$eps)/$N)->slice("*1,");
+  my $hb  = -log2(($fb+$eps)/$N);
 
-    $Ptb *= log($Ptb/($Pt*$Pb))/log(2);
+  foreach my $z (0,1) {
+    my $zpdl = $pdl->slice("($z)");             ##-- [b,w] -> f(b,w) [--> PROFILE(w,b)]
+
+    my $hbw = -log2(($zpdl+$eps)/$N);           ##-- [b,w] -> h(b,w)
+    $zpdl  .= $hw + $hb - $hwb;
   }
   $pdl->inplace->setnantobad->inplace->setbadtoval(0);
 
@@ -68,13 +73,13 @@ sub finishPdl {
 sub helpString {
   my $that = shift;
   return
-    (qq(Extract L/R- joint-weighted MI profile wrt. fixed boundary set.\n)
+    (qq(Extract L/R- relative pointwise MI PROFILE wrt. fixed boundary set.\n)
      .qq(Options:\n)
      .qq(  bounds=ENUM      [default=empty]\n)
      .qq(  targets=ENUM     [default=empty]\n)
      .qq(  eos=EOS_STRING   [default='__\$']\n)
      .qq(  bos=BOS_STRING   [default='__\$']\n)
-     .qq(  donorm=BOOL      [default=1]\n)
+     .qq(...\n)
     );
 }
 
